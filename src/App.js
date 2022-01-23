75d644062d18d789576979c774e9e73b51b99d44
somporn <sompor222@gmail.com>
2022-01-23 21:43:53 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import {Route ,Routes} from 'react-router-dom'
+import Home from './Home';
+import About from './About';
+import Service from './Service';
+import Contact from './Contact';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" exact={true} element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/service" element={<Service />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom'
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './style/custom.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
